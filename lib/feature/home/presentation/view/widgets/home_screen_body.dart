978838d35cf8039b92a72b64ad7e6448cc225deb
Ebrahim-Elkbbany <ana.elkbany@gmail.com>
2023-08-13 05:00:07 +0200

    create categories view & finish ui

--- lib/feature/home/presentation/view/widgets/home_screen_body.dart
+++ lib/feature/home/presentation/view/widgets/home_screen_body.dart
@@ -1,8 +1,4 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:fip/core/utils/constant.dart';
-import 'package:fip/feature/home/presentation/view/widgets/home_categories_list_view_item.dart';
 import 'package:fip/feature/home/presentation/view/widgets/home_categories_section.dart';
-import 'package:fip/feature/home/presentation/view/widgets/home_products_grid_view_item.dart';
 import 'package:fip/feature/home/presentation/view/widgets/home_products_section.dart';
 import 'package:fip/feature/home/presentation/view/widgets/home_slider_section.dart';
 import 'package:flutter/material.dart';
